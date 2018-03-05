f85f4abbaec97eaf300b07ac2661b4aae110fc8a
eun1310434 <eun1310434@naver.com>
2018-03-05 16:22:46 +0900

    BroadcastReceiver checkSelfPermission

---
+++ app/src/main/java/com/eun1310434/receiver/MainActivity.java
@@ -0,0 +1,88 @@
+/**
+ * 20.02.2018
+ * eun1310434@naver.com
+ * https://blog.naver.com/eun1310434
+ * 참고) Do it android programming
+ */
+
+package com.eun1310434.receiver;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    //sms message receive
+    public static final int REQUEST_CODE_SMS = 201;
+    TextView tv_Log;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tv_Log = (TextView) findViewById(R.id.tv_Log);
+
+        // 위험권한 : 일전에 manifast를 통해서 권한을 승인 받은 것에 추가로, 앱 실행시 권한요청 보여줌(Sdk23 이후 버젼).
+        // 대표 위험권한 : 위치, 카메라, 마이크, 연락처, 전화, 문자, 일정, 센서
+        int permissionCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.RECEIVE_SMS);
+
+        if (permissionCheck == PackageManager.PERMISSION_GRANTED) {//SNS 수신 권한 이미 승인 한 경우
+            Toast.makeText(this, "SMS PERMISSION_GRANTED.", Toast.LENGTH_SHORT).show();
+            tv_Log.setText(tv_Log.getText()+"SMS PERMISSION_GRANTED. - SNS 수신 권한 이미 승인 한 경우\n\n");
+
+        } else {//SNS 수신 권한 미승인 한 경우
+            Toast.makeText(this, "SMS PERMISSION NOT GRANTED.", Toast.LENGTH_SHORT).show();
+            tv_Log.setText(tv_Log.getText()+"SMS PERMISSION NOT GRANTED. - SNS 수신 권한 미승인 한 경우\n\n");
+
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.RECEIVE_SMS)) {
+                Toast.makeText(this, "NEEDS TO SNS PERMISSION EXPLAIN", Toast.LENGTH_SHORT).show();
+                tv_Log.setText(tv_Log.getText()+"NEEDS TO SNS PERMISSION EXPLAIN - SNS 수신 권한을 거부 하면 관련 설명 필요함을 표시\n\n");
+                //SNS 수신 권한을 거부 하면 관련 설명 필요함을 표시
+
+            } else {
+                ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.RECEIVE_SMS}, 1);
+                //onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) 을 호출
+                //requestCode에 맞춰서 호출해야 함. 현재 이곳에서는 1로 호출
+
+            }
+        }
+
+        Button button = (Button) findViewById(R.id.btn_goToSmsActivity);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), SmsActivity.class);
+                startActivityForResult(intent, REQUEST_CODE_SMS);
+                finish();
+            }
+        });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        //권한 승인 선택에 따른 설정
+        switch (requestCode) {
+            case 1: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(this, "SMS PERMISSION AGREE", Toast.LENGTH_SHORT).show();
+                    tv_Log.setText(tv_Log.getText()+"SMS PERMISSION AGREE - SMS 권한을 사용자가 승인함.\n\n");
+                    //SMS 권한을 사용자가 승인함.
+                } else {
+                    Toast.makeText(this, "SMS PERMISSION DISAGREE", Toast.LENGTH_SHORT).show();
+                    tv_Log.setText(tv_Log.getText()+"SMS PERMISSION DISAGREE - SMS 권한 거부됨.\n\n");
+                    //SMS 권한 거부됨.
+                }
+                return;
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/eun1310434/receiver/SmsReceiver.java
@@ -0,0 +1,84 @@
+/**
+ * 05.03.2018
+ * eun1310434@naver.com
+ * https://blog.naver.com/eun1310434
+ * 참고) Do it android programming
+ */
+
+package com.eun1310434.receiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.telephony.SmsMessage;
+import android.util.Log;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class SmsReceiver extends BroadcastReceiver {
+    //SMS 메시지를 받아 처리하기 위한 수신자
+    public static final String TAG = "SmsReceiver";
+
+    public SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Log.i(TAG, "onReceive()");
+
+        Bundle bundle = intent.getExtras();// 메세지 수신 시 내용 Bundle 로 파싱
+        SmsMessage[] messages = parseSmsMessage(bundle);
+
+        if (messages != null && messages.length > 0) {
+            String sender = messages[0].getOriginatingAddress();// SMS 발신 번호 확인
+            Log.i(TAG, "SMS sender : " + sender);
+
+            String contents = messages[0].getMessageBody().toString();// SMS 메시지 확인
+            Log.i(TAG, "SMS contents : " + contents);
+
+            Date receivedDate = new Date(messages[0].getTimestampMillis());// SMS 수신 시간 확인
+            Log.i(TAG, "SMS received date : " + receivedDate.toString());
+
+            sendToActivity(context, sender, contents, receivedDate);
+        }
+
+    }
+
+
+    private SmsMessage[] parseSmsMessage(Bundle bundle) {//SMS 메시지 파싱
+
+        Object[] objs = (Object[]) bundle.get("pdus");// 국제표준 토콜 : pdus
+
+        SmsMessage[] messages = new SmsMessage[objs.length];
+        for (int i = 0; i < objs.length; i++) {
+            // PDU 포맷 - 메시지 복원
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                // API 23 이상 시 format Setting 필요
+                String format = bundle.getString("format");
+                messages[i] = SmsMessage.createFromPdu((byte[]) objs[i], format);
+
+            } else {
+                messages[i] = SmsMessage.createFromPdu((byte[]) objs[i]);
+            }
+        }
+        return messages;
+    }
+
+    private void sendToActivity(Context context, String sender, String contents, Date receivedDate) {
+        Intent myIntent = new Intent(context, SmsActivity.class);
+
+        myIntent.addFlags(
+                Intent.FLAG_ACTIVITY_NEW_TASK|
+                Intent.FLAG_ACTIVITY_SINGLE_TOP|
+                Intent.FLAG_ACTIVITY_CLEAR_TOP);
+
+        myIntent.putExtra("sender", sender);
+        myIntent.putExtra("contents", contents);
+        myIntent.putExtra("receivedDate", format.format(receivedDate));
+
+        context.startActivity(myIntent);
+    }
+
+}
